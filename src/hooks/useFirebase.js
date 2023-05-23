da28842b837be3da1209b8e26ac390ba7fb37c31
MarianoMartini22 <mayu20036@hotmail.com>
2023-05-22 21:37:20 -0300

    muestro loader

--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,8 +1,9 @@
-import { collection, getDocs, getDoc, doc, where, query } from "firebase/firestore";
+import Swal from 'sweetalert2';
+import { collection, getDocs, getDoc, doc, where, query } from 'firebase/firestore';
 import { db } from '../firebase';
 import { useEffect, useState } from 'react';
 
-export const useFirebase = (by = "id", params = {}, coll, find = false, refresh = false) => {
+export const useFirebase = (by = 'id', params = {}, coll, find = false, refresh = false) => {
   const [dataQuery, setDataQuery] = useState([]);
   const condition = (refresh && params.category);
   useEffect(() => {
@@ -11,7 +12,16 @@ export const useFirebase = (by = "id", params = {}, coll, find = false, refresh 
     const fetchData = async () => {
       try {
         if (!find) {
-          const querySnapshot = await getDocs(collection(db, "items"));
+          Swal.fire({
+            title: 'Cargando...',
+            html: 'Obteniendo datos de la base de datos',
+            allowOutsideClick: false,
+            didOpen: () => {
+              Swal.showLoading();
+            },
+          });
+
+          const querySnapshot = await getDocs(collection(db, 'items'));
           const newData = querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
           if (isMounted) {
             setDataQuery(newData);
@@ -22,7 +32,15 @@ export const useFirebase = (by = "id", params = {}, coll, find = false, refresh 
 
           switch (by) {
             case 'categorias':
-              const q = query(collection(db, coll), where("category", "==", +params.category));
+              Swal.fire({
+                title: 'Cargando...',
+                html: 'Obteniendo datos de la base de datos',
+                allowOutsideClick: false,
+                didOpen: () => {
+                  Swal.showLoading();
+                },
+              });
+              const q = query(collection(db, coll), where('category', '==', +params.category));
               querySnapshot = await getDocs(q);
               newData = querySnapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id }));
               if (isMounted) {
@@ -30,18 +48,35 @@ export const useFirebase = (by = "id", params = {}, coll, find = false, refresh 
               }
               break;
             case 'id':
+              Swal.fire({
+                title: 'Cargando...',
+                html: 'Obteniendo datos de la base de datos',
+                allowOutsideClick: false,
+                didOpen: () => {
+                  Swal.showLoading();
+                },
+              });
               let docRefId = doc(db, coll, params.id);
               let docSnapId = await getDoc(docRefId);
+
               if (docSnapId.exists()) {
                 if (isMounted) {
                   setDataQuery(docSnapId.data());
                 }
               } else {
-                setDataQuery([{message: 'No existe el item seleccionado'}])
+                setDataQuery([{ message: 'No existe el item seleccionado' }]);
               }
               break;
             case 'categoria':
               if (params.id) {
+                Swal.fire({
+                  title: 'Cargando...',
+                  html: 'Obteniendo datos de la base de datos',
+                  allowOutsideClick: false,
+                  didOpen: () => {
+                    Swal.showLoading();
+                  },
+                });
                 let docRefCategoria = doc(db, coll, params.id.toString());
                 let docSnapCategoria = await getDoc(docRefCategoria);
                 if (docSnapCategoria.exists()) {
@@ -49,7 +84,7 @@ export const useFirebase = (by = "id", params = {}, coll, find = false, refresh 
                     setDataQuery(docSnapCategoria.data());
                   }
                 } else {
-                  console.error("La categoria no existe");
+                  console.error('La categoria no existe');
                 }
               }
               break;
@@ -59,10 +94,13 @@ export const useFirebase = (by = "id", params = {}, coll, find = false, refresh 
         }
       } catch (error) {
         console.error(error);
+      } finally {
+        Swal.close();
       }
     };
 
     fetchData();
+
     return () => {
       isMounted = false;
     };
